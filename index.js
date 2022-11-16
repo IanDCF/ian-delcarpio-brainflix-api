ae948497bb44f67e09e88d636422876bd87c7945
Ian DC <jokerzamba@hotmail.com>
2022-11-15 21:07:27 -0800

    Test backend axios call

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
+const apiKey = process.env.API_KEY;
+const domain = process.env.API_DOMAIN;
+const videosUrl = `https://${domain}/videos?api_key=${apiKey}`;
+
 app.get("/", (_req, res) => {
+  axios.get(videosUrl).then((response) => {
+    console.log(response.data);
+  });
   res.send("WELCOME");
 });
 
