a44839cdd2883aab718569108dffc385af81f2c1
Ian DC <jokerzamba@hotmail.com>
2022-11-17 20:21:44 -0800

    Test server Successfully

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("node:path");
+require("dotenv").config();
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 const videoListJSONFile = path.join(__dirname, "./data/videoList.json");
 const videoList = require(videoListJSONFile);
@@ -8,7 +12,9 @@ const videoList = require(videoListJSONFile);
 const videoDetailsJSONFile = path.join(__dirname, "./data/videoDetails.json");
 const videoDetails = require(videoDetailsJSONFile);
 
-console.log(videoDetails);
+const PORT = process.env.PORT;
+
+app.use(express.static("public"));
 
 app.get("/videos", (_req, res) => {
   res.status(200).json(videoList);
@@ -24,6 +30,6 @@ app.get("/videos/:videoId", (req, res) => {
 app.get("/", (_req, res) => {
   res.send("WELCOME");
 });
-app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
